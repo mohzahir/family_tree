d9acb5ebae1122d5bb2dde3cba49799de85298ef
moh-zahir <moh.zahir.mz@gmail.com>
2022-06-22 21:14:32 +0200

    production

--- family-tree/app/Providers/AppServiceProvider.php
+++ family-tree/app/Providers/AppServiceProvider.php
@@ -24,10 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('REDIRECT_HTTPS')) {
-            \URL::forceScheme('https');
-        }
-
         Schema::defaultStringLength(191);
     }
 }
